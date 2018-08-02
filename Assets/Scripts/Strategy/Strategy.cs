b2e7b2e5bb5920ad725fe4bce115c1373f312594
lz0928 <1137556826@qq.com>
2018-08-02 11:15:52 +0800

    Initial commit

---
+++ Assets/Scripts/Strategy/Strategy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Strategy
+{
+
+    public class CashStrategyContext
+    {
+        CashSuper cs;
+        public CashStrategyContext(string type)
+        {
+            switch (type)
+            {
+                case "正常收费":
+                    cs = new CashNormal();
+                    break;
+                case "满300减100":
+                    cs = new CashReturn(300, 100);
+                    break;
+                case "打八折":
+                    cs = new CashRebate(0.8f);
+                    break;
+                default:
+                    Debug.LogWarning("type:" + type);
+                    break;
+            }
+        }
+
+        public float GetResult(float money)
+        {
+            return cs.acceptCash(money);
+        }
+    }
+
+    /// <summary>
+    /// 收费抽象类
+    /// </summary>
+    public abstract class CashSuper
+    {
+        public abstract float acceptCash(float money);
+    }
+    
+    public class CashNormal : CashSuper
+    {
+        public override float acceptCash(float money)
+        {
+            return money;
+        }
+    }
+
+    public class CashRebate : CashSuper
+    {
+        private float m_retate;
+        public CashRebate(float rebate)
+        {
+            m_retate = rebate;
+        }
+        public override float acceptCash(float money)
+        {
+            return m_retate * money;
+        }
+    }
+
+    public class CashReturn : CashSuper
+    {
+        private float monoeyCondition;
+        private float moneyReturn;
+
+        public CashReturn(float condition, float mreturn)
+        {
+            monoeyCondition = condition;
+            moneyReturn = mreturn;
+        }
+        public override float acceptCash(float money)
+        {
+            float result = money;
+            if(money >= monoeyCondition)
+            {
+                result = money - (float)Math.Floor(money / monoeyCondition) * moneyReturn;
+            }
+            return result;
+        }
+    }
+}
---
+++ Assets/Scripts/Strategy/StrategyClient.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Strategy
+{
+    public class StrategyClient : MonoBehaviour
+    {
+        public InputField priceInput;
+        public InputField countInput;
+        public Dropdown calWay;
+
+        public Text showLabel;
+        public Text totalText;
+
+        private float total;
+
+        public void onBtnConfirmClick()
+        {
+            int price = int.Parse(priceInput.text);
+            int count = int.Parse(countInput.text);
+            CashStrategyContext csc = new CashStrategyContext(calWay.transform.Find("Label").GetComponent<Text>().text);
+
+            float totalPrice = 0;
+            totalPrice = csc.GetResult(price * count);
+            total += totalPrice;
+            showLabel.text = showLabel.text + "\n" + "单价：" + price + " 数量：" + count + " " +
+                calWay.itemText.text + " 总计：" + totalPrice;
+            totalText.text = total.ToString();
+        }
+
+        public void onBtnCanelClick()
+        {
+            
+        }
+    }
+}
